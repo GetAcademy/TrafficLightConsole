a7eb753f3b4c0c9a0e6088eb07320049fb1e3e98
GET Academy <terje@getacademy.no>
2021-11-02 09:54:44 +0100

    Initial commit

---
+++ TrafficLight/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace TrafficLight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("┏━━┓");
+            DrawLine(ConsoleColor.Red);
+            DrawLine(ConsoleColor.Yellow);
+            DrawLine(ConsoleColor.Green);
+            Console.WriteLine("┗━━┛");
+        }
+
+        public static void DrawLine(ConsoleColor color)
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("┃");
+            Console.ForegroundColor = color;
+            Console.Write("⚫");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("┃");
+        }
+    }
+}
