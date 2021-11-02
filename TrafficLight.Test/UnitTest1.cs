4dc22493b4cebf552a7dee82c2ff611fc608e6b5
GET Academy <terje@getacademy.no>
2021-11-02 11:27:17 +0100

    x

---
+++ TrafficLight.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace TrafficLight.Test
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(1,1);
+        }
+    }
+}
---
+++ TrafficLight/TrafficLightView.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrafficLight
+{
+    class TrafficLightView
+    {
+        static void Mainx(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("┏━━┓");
+            DrawLine(ConsoleColor.Red);
+            DrawLine(ConsoleColor.Yellow);
+            DrawLine(ConsoleColor.Green);
+            Console.WriteLine("┗━━┛");
+        }
+
+        public static void DrawLine(ConsoleColor color)
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("┃");
+            Console.ForegroundColor = color;
+            Console.Write("⚫");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("┃");
+        }
+    }
+}
